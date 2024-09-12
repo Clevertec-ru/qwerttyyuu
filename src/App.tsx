6a72d33044d4df203721cb68adc660c5ccf035f1
Татьяна Иванова <tati.ivanova.87@gmail.com>
2024-09-12 14:21:52 +0300

    fix: delete button

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import { ReactFlowProvider } from '@xyflow/react';
-import { useState } from 'react';
 
-import styles from './App.module.css';
 import { Diagram } from './components/diagram';
 
 function App() {
-  const [isDiagramVisible, setIsDiagramVisible] = useState(true);
-
-  const toggleDiagram = () => setIsDiagramVisible((prev) => !prev);
-
   return (
     <ReactFlowProvider>
-      <button className={styles.demoButton} onClick={toggleDiagram}>
-        {isDiagramVisible ? 'Hide Diagram' : 'Show Diagram'}
-      </button>
-      {isDiagramVisible && <Diagram />}
+      <Diagram />
     </ReactFlowProvider>
   );
 }

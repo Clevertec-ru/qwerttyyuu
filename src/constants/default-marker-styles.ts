ef4cb4caa78bbd34713b80307b53cf73d85d768b
Татьяна Иванова <tati.ivanova.87@gmail.com>
2024-09-13 12:53:02 +0300

    mend

--- src/constants/default-marker-styles.ts
+++ src/constants/default-marker-styles.ts
@@ -1,6 +1,6 @@
 import { MarkerType } from '@xyflow/react';
 
-const markerStyles = { width: 20, height: 30, color: '#ff4d41', type: MarkerType.ArrowClosed };
+const markerStyles = { width: 20, height: 30, color: 'var(--demo-attention)', type: MarkerType.ArrowClosed };
 
 export const defaultMarkerStyles = {
   markerStart: markerStyles,

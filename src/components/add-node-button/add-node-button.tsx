e4a16edd951ca22ab99b11af329782cc1cd94488
skustov <skustov@clevertec.ru>
2024-09-13 09:21:47 +0300

    feat(*): provide adding node functionality

--- src/components/add-node-button/add-node-button.tsx
+++ src/components/add-node-button/add-node-button.tsx
@@ -1,14 +1,17 @@
 import { CSSProperties, FC } from 'react';
 
 import imgAdd from '../../assets/images/addIcon.png';
+import { useAddSameNode } from '../../hooks/use-add-same-node';
 
 import styles from './add-node-button.module.css';
 
 type AddNodeButtonProps = { id: string } & CSSProperties;
 
 export const AddNodeButton: FC<AddNodeButtonProps> = ({ id, ...stylesProps }) => {
+  const { addNewNode } = useAddSameNode(id);
+
   return (
-    <button onClick={() => console.log(id)} className={styles.addButton} style={stylesProps}>
+    <button onClick={() => addNewNode()} className={styles.addButton} style={stylesProps}>
       <img src={imgAdd} alt='add node' className={styles.image} />
     </button>
   );
--- src/hooks/use-add-same-node.ts
+++ src/hooks/use-add-same-node.ts
@@ -1,8 +1,23 @@
 import { useReactFlow } from '@xyflow/react';
-import { useId } from 'react';
+import { useCallback, useId } from 'react';
 
-export const UseAddSameNode = (id: string) => {
+export const useAddSameNode = (id: string) => {
   const uniqueId = useId();
-  const { addNodes, addEdges, getNode } = useReactFlow();
-  const currentNode = getNode(id);
+  const { addNodes, getNode } = useReactFlow();
+  const currNode = getNode(id);
+
+  const addNewNode = useCallback(() => {
+    if (!currNode) return;
+    const newNode = {
+      ...currNode,
+      id: uniqueId,
+      position: {
+        x: currNode?.position ? currNode.position.x + 40 : 40,
+        y: currNode?.position ? currNode.position.y + 20 : 20,
+      },
+    };
+    addNodes(newNode);
+  }, []);
+
+  return { addNewNode };
 };

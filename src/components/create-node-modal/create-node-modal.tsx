21bd1f021ebbc136100ebf1f4ca53ee09a5103c3
Татьяна Иванова <tati.ivanova.87@gmail.com>
2024-09-17 10:50:16 +0300

    feat: add sidebar

--- src/components/create-node-modal/create-node-modal.tsx
+++ src/components/create-node-modal/create-node-modal.tsx
@@ -11,16 +11,12 @@ import styles from './create-node-modal.module.css';
 type CreateNodeModalProps = {
   setIsModalOpen: (isOpen: boolean) => void;
   isModalOpen: boolean;
-}
+};
 
 export const CreateNodeModal = ({ setIsModalOpen, isModalOpen }: CreateNodeModalProps) => {
   const { addNewNode } = useAddNodeBySettings();
   const [uniqueId, setUniqueId] = useState(20);
-  const {
-    register,
-    handleSubmit,
-    reset,
-  } = useForm<CreateNodeType>();
+  const { register, handleSubmit, reset } = useForm<CreateNodeType>();
   const [selectedVariant, setSelectedVariant] = useState<NodeUiVariants>(NodeUiVariants.Rectangle);
 
   const handleShapeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -28,9 +24,8 @@ export const CreateNodeModal = ({ setIsModalOpen, isModalOpen }: CreateNodeModal
     setSelectedVariant(selected);
   };
 
-
   const onSubmit: SubmitHandler<CreateNodeType> = (data) => {
-    setUniqueId(prevState => prevState + 1);
+    setUniqueId((prevState) => prevState + 1);
     addNewNode({ ...data, uniqueId: uniqueId.toString() });
     setIsModalOpen(false);
     reset();
@@ -46,50 +41,57 @@ export const CreateNodeModal = ({ setIsModalOpen, isModalOpen }: CreateNodeModal
   //   </option>
   // ))
 
-
   return (
-    isModalOpen && <div className={styles.modal} onClick={handleClose}>
-      <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
-        <form onSubmit={handleSubmit(onSubmit)} className={styles.container}>
-          <label>Text</label>
-          <input className={styles.input} type="text" {...register('nodeText', { required: true })} />
-          <label>Shape</label>
-          <select className={styles.select} {...register('nodeShape', { required: true })} onChange={handleShapeChange}
-                  value={selectedVariant}>
-            {Object.values(NodeUiVariants).map((variant) => (
-              <option key={variant} value={variant}>
-                {variantNames[variant]}
-              </option>
-            ))}
-          </select>
-          <div className={styles.nodePreview}>
-            <SwitchedUiComponent variant={selectedVariant} />
-          </div>
-          <label>Top dots count</label>
-          {(selectedVariant === NodeUiVariants.Rectangle || selectedVariant === NodeUiVariants.RectangleOutlined) &&
-            <div className={styles.container}><input className={styles.input}
-                                                type={'number'} {...register('topDotsCount')} />
-              {/*<label>Top dots type</label>*/}
-              {/*<select {...register('topDotsType')}>{handleTypes()}</select>*/}
-              <label>Left dots count</label>
-              <input className={styles.input} type={'number'} {...register('leftDotsCount')} />
-              {/*<label>Left dots type</label>*/}
-              {/*<select {...register('leftDotsType')}>{handleTypes()}</select>*/}
-              <label>Bottom dots count</label>
-              <input className={styles.input} type={'number'} {...register('botDotsCount')} />
-              {/*<label>Bottom dots type</label>*/}
-              {/*<select {...register('botDotsType')}>{handleTypes()}</select>*/}
-              <label>Right dots count</label>
-              <input className={styles.input} type={'number'} {...register('rightDotsCount')} />
-              {/*<label>Right dots type</label>*/}
-              {/*<select {...register('rightDotsType')}>{handleTypes()}</select>*/}</div>}
-          <label>Height</label>
-          <input className={styles.input} type={'number'} {...register('height', { required: true })}></input>
-          <label>Width</label>
-          <input className={styles.input} type={'number'} {...register('width', { required: true })}></input>
-          <button type={'submit'}>submit</button>
-        </form>
+    isModalOpen && (
+      <div className={styles.modal} onClick={handleClose}>
+        <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+          <form onSubmit={handleSubmit(onSubmit)} className={styles.container}>
+            <label>Text</label>
+            <input className={styles.input} type='text' {...register('nodeText', { required: true })} />
+            <label>Shape</label>
+            <select
+              className={styles.select}
+              {...register('nodeShape', { required: true })}
+              onChange={handleShapeChange}
+              value={selectedVariant}
+            >
+              {Object.values(NodeUiVariants).map((variant) => (
+                <option key={variant} value={variant}>
+                  {variantNames[variant]}
+                </option>
+              ))}
+            </select>
+            <div className={styles.nodePreview}>
+              <SwitchedUiComponent variant={selectedVariant} />
+            </div>
+            <label>Top dots count</label>
+            {(selectedVariant === NodeUiVariants.Rectangle || selectedVariant === NodeUiVariants.RectangleOutlined) && (
+              <div className={styles.container}>
+                <input className={styles.input} type={'number'} {...register('topDotsCount')} />
+                {/*<label>Top dots type</label>*/}
+                {/*<select {...register('topDotsType')}>{handleTypes()}</select>*/}
+                <label>Left dots count</label>
+                <input className={styles.input} type={'number'} {...register('leftDotsCount')} />
+                {/*<label>Left dots type</label>*/}
+                {/*<select {...register('leftDotsType')}>{handleTypes()}</select>*/}
+                <label>Bottom dots count</label>
+                <input className={styles.input} type={'number'} {...register('botDotsCount')} />
+                {/*<label>Bottom dots type</label>*/}
+                {/*<select {...register('botDotsType')}>{handleTypes()}</select>*/}
+                <label>Right dots count</label>
+                <input className={styles.input} type={'number'} {...register('rightDotsCount')} />
+                {/*<label>Right dots type</label>*/}
+                {/*<select {...register('rightDotsType')}>{handleTypes()}</select>*/}
+              </div>
+            )}
+            <label>Height</label>
+            <input className={styles.input} type={'number'} {...register('height', { required: true })}></input>
+            <label>Width</label>
+            <input className={styles.input} type={'number'} {...register('width', { required: true })}></input>
+            <button type={'submit'}>submit</button>
+          </form>
+        </div>
       </div>
-    </div>
+    )
   );
 };
--- src/components/custom-panel/custom-panel.tsx
+++ src/components/custom-panel/custom-panel.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { Panel, PanelPosition } from '@xyflow/react';
 
-import styles from '../create-node-modal/create-node-modal.module.css';
 import { CreateNodeModal } from '../create-node-modal/create-node-modal.tsx';
+import { CustomButton } from '../custom-button/custom-button.tsx';
+
+import styles from './custom-panel.module.css';
 
 type CustomPanelProps = {
   position: PanelPosition;
@@ -10,11 +12,12 @@ type CustomPanelProps = {
 
 export const CustomPanel: React.FC<CustomPanelProps> = ({ position }) => {
   const [isModalOpen, setModalOpen] = useState(false);
- 
 
   return (
-    <Panel position={position}>
-      <button className={styles.openBtn} onClick={() =>setModalOpen(!isModalOpen)}>add block</button>
+    <Panel position={position} className={styles.openBtn}>
+      <CustomButton className={styles.openBtn} onClick={() => setModalOpen(!isModalOpen)}>
+        add block
+      </CustomButton>
       <CreateNodeModal isModalOpen={isModalOpen} setIsModalOpen={setModalOpen}></CreateNodeModal>
     </Panel>
   );
